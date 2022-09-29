577dcf402f5d00a47298898480b1736add64ca70
ramin zamani <zamaniramin549@gmail.com>
2022-09-29 19:55:02 +0800

    chnages

--- saleduck_scraping/genre.js
+++ saleduck_scraping/genre.js
@@ -1,32 +1,38 @@
 //h4.split('coupon code:')[1].trim().split('Available')[0]
 // sentence.slice(sentence.indexOf('coupon code:') + 'coupon code:'.length).trim().split(' ')[0].replace('Available', '')
 
+import fetch from 'node-fetch';
+import * as cheerio from "cheerio";
+import { Qyu } from 'qyu';
 
-const cheerio = require("cheerio");;
-const axios = require("axios");
 
-const url="https://www.hp.com/id-en/shop/";
-
-const data_code = []
-
-async function getGenre(){
-
-    try{
-
-        const response = await axios.get(url);
-        const $ = cheerio.load(response.data);
-        const h4 = $("h4").text();
-        // const coupon = h4.includes('coupon code')  
-        coupon = h4.slice(h4.indexOf('coupon code:') + 'coupon code:'.length).trim().split(' ')[0].replace('Available', '')
-        console.log(coupon);
-
-                
+(async () => {
+    const siteUrl = ['https://www.hp.com/id-en/shop/', "https://www.ramzamani.com/", "https://www.asos.com/"];
+    const q = new Qyu({concurrency: 3});
+    // const len= siteUrl.length;
+    for (let key in siteUrl) {
+        q(async () => {
+            const resp = await fetch(siteUrl[key]);
+            const html = await resp.text();
+            const $ = cheerio.load(html);
+            const h4 = $("h4").text();
+            // const coupon = h4.includes('coupon code')  
+            const coupon = h4.slice(h4.indexOf('coupon code:') + 'coupon code:'.length).trim().split(' ')[0].replace('Available', '')
+            console.log(coupon);
+            console.log(siteUrl[key]);
+            
+            // let products = [];
+            // $('.product-list .product').each((i, elem) => {
+            //     let $elem = $(elem);
+            //     let title = $elem.find('.title').text();
+            //     let price = $elem.find('.price').text();
+            //     products.push({ title, price });
+            // });
+            // Do something with products...
+        });
     }
 
-    catch(error){
-        console.error(error);
-    }
+    await q.whenEmpty();
 
-}
-
-getGenre();
+    // All done!
+})();
